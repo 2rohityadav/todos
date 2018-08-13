b1c6136e5a9e36e07e258e28442d3e9735e94949
rohyadav <rohityadav.techie@gmail.com>
2018-08-14 00:10:57 +0530

    br todo-redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+//// redux imports
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { logger } from "redux-logger";
+
+//? redux-devtool-extension
+import { composeWithDevTools } from "redux-devtools-extension";
+
 import App from "./components/App";
+import { todoReducer } from "./redux-store/Store";
 
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+//! store
+const store = createStore(
+  todoReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("app")
+);
